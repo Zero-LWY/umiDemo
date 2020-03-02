039fd3df307dbebcfb2ea9592d09c56887a168f6
Zero灬 <1243426954@163.com>
2020-03-02 18:07:23 +0800

    [ADD] 路由过滤

--- src/layouts/SecurityLayout.jsx
+++ src/layouts/SecurityLayout.jsx
@@ -3,6 +3,7 @@ import { connect } from 'dva';
 import { PageLoading } from '@ant-design/pro-layout';
 import { Redirect } from 'umi';
 import { stringify } from 'querystring';
+import { getAuthority } from '@/utils/authority';
 
 class SecurityLayout extends React.Component {
   state = {
@@ -26,18 +27,19 @@ class SecurityLayout extends React.Component {
     const { isReady } = this.state;
     const { children, loading, currentUser } = this.props; // You can replace it to your authentication rule (such as check token exists)
     // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
-
+    const authority = getAuthority();
     const isLogin = currentUser && currentUser.userid;
+    console.log(isLogin);
     const queryString = stringify({
       redirect: window.location.href,
     });
 
-    if ((!isLogin && loading) || !isReady) {
-      return <PageLoading />;
+    if (!authority && window.location.pathname !== '/user/login') {
+      return <Redirect to={`/user/login?${queryString}`} />;
     }
 
-    if (!isLogin && window.location.pathname !== '/user/login') {
-      return <Redirect to={`/user/login?${queryString}`} />;
+    if ((!isLogin && loading) || !isReady) {
+      return <PageLoading />;
     }
 
     return children;
